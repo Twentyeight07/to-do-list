15880ecd9d05ca5c75b546837959797649db7ce4
Twentyeight07 <eduardofreites1999@gmail.com>
2022-10-07 20:08:07 -0400

    SW changes

--- sw.js
+++ sw.js
@@ -2,7 +2,6 @@
 
 const CACHE_NAME = "v1_cache_todo_list",
   urlsToCache = [
-    "https://twentyeight07.github.io/to-do-list/",
     "https://twentyeight07.github.io/to-do-list/index.html",
     "https://twentyeight07.github.io/to-do-list/style.css",
     "https://twentyeight07.github.io/to-do-list/js/script.js",
