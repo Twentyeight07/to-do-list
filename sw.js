a276b0cc0eb38b0e2258e34396e26b00bcc9e9eb
Twentyeight07 <eduardofreites1999@gmail.com>
2022-10-07 20:18:10 -0400

    testing changes in SW

--- sw.js
+++ sw.js
@@ -1,12 +1,7 @@
 //Asignar un nombre y versión al caché
 
 const CACHE_NAME = "v1_cache_todo_list",
-  urlsToCache = [
-    "https://twentyeight07.github.io/to-do-list/index.html",
-    "https://twentyeight07.github.io/to-do-list/style.css",
-    "https://twentyeight07.github.io/to-do-list/js/script.js",
-    "https://twentyeight07.github.io/to-do-list/assets/favicon.png",
-  ];
+  urlsToCache = ["/index.html", "/style.css", "/js", "/assets/favicon.png"];
 
 const preCache = async () => {
   const cache = await caches.open(CACHE_NAME);
